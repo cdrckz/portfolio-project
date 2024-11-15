2cf0f7401bce488ac817d7865051dfd19197bd4f
cdrckz <cedrick.puse@tup.edu.ph>
2024-11-15 13:10:32 +0800

    thank you message
    Added a thank you message when you submit a message

--- ASSETS/script.js
+++ ASSETS/script.js
@@ -6,3 +6,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
     });
   });
+
+document.getElementById('contactForm').addEventListener('submit', function(event){
+  event.preventDefault(); 
+
+  alert("Thank you for your message :)");
+  window.location.href ='index.html'; 
+}); 
